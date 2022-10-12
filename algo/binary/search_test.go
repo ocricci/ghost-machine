8104aff453563d3f4bc7bd338fae779277bc71a5
ocricci <oc.ricci@gmail.com>
2022-10-12 14:26:54 -0300

    feat(binary/search): add binary search test

--- algo/binary/search_test.go
+++ algo/binary/search_test.go
@@ -6,15 +6,35 @@ import (
 )
 
 func searchTestCases() [][]int {
-	return [][]int{[]int{1, 2, 3, 4}, []int{1, 2, 3, 4, 5, 6, 7}}
+	return [][]int{
+		[]int{1, 2, 3, 4},
+		[]int{1, 2, 3, 4, 5, 6, 7},
+		[]int{70, 80, 90, 100, 1000, 9000},
+	}
 }
 
 func TestSearchExistingNumber(t *testing.T) {
 	for _, testCase := range searchTestCases() {
 		m := uint(math.Floor(float64(len(testCase) / 2)))
-		result := Search(testCase, testCase[m])
-		if !result {
-			t.Errorf("search existing number failed")
+		if !Search(testCase, testCase[m]) {
+			t.Errorf("Unable to find number in the middle")
+		}
+
+		if !Search(testCase, testCase[0]) {
+			t.Error("Unable to find the first occurrence")
+		}
+
+		if !Search(testCase, testCase[len(testCase) -1]) {
+			t.Error("Unable to find the last occurrence")
+		}
+
+	}
+}
+
+func TestSearchDeadNumber(t *testing.T) {
+	for _, testCase := range searchTestCases() {
+		if Search(testCase, testCase[len(testCase) - 1] + 1) {
+			t.Errorf("Found a dead number !")
 		}
 	}
 }
